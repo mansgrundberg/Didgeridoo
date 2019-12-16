5bacc04765be81c743dcab9d9f11934426325d24
Måns Grundberg <mans.grundberg@gmail.com>
2019-12-16 10:09:47 +0100

    Hej

--- src/Backend/APIRunner.java
+++ src/Backend/APIRunner.java
@@ -1,5 +1,19 @@
 package Backend;
 
-public class APIRunner {
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
 
+import com.google.gson.Gson;
+
+
+public class APIRunner {
+	public static void main(String[] args) throws Exception {
+		port(5000);
+		
+		get("/:id", (req, res) -> {
+			
+			return "";
+		});
+	}
 }
