6a21a613277d1abab75cc2ce5fb4f9fa766c3ed6
Måns Grundberg <mans.grundberg@gmail.com>
2019-12-16 11:31:25 +0100

    SpotifyConsumer

--- src/Backend/SpotifyConsumer.java
+++ src/Backend/SpotifyConsumer.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 
 public class SpotifyConsumer {
 
-    private static void fetchResult(String client_id, String client_secret) {
+    private static void fetchToken(String client_id, String client_secret) {
         try {
             HttpResponse<JsonNode> response = Unirest.post("https://accounts.spotify.com/api/token")
-                    .header("Authorization","Basic <base64 encoded " + client_id + ":"+client_secret+">")
+            		.basicAuth(client_id, client_secret)
                     .field("grant_type","client_credentials")
                     .asJson();
             System.out.println(response.getBody());
@@ -25,7 +25,7 @@ public class SpotifyConsumer {
     public static void main(String[] args) {
         String client_id = "fb435518dac2475388699e465c3ad740";
         String client_secret = "e4f91cb42afc40cbba822bd73b81801b";
-        fetchResult(client_id,client_secret);
+        fetchToken(client_id,client_secret);
     }
 
 }
