04ffee0c32ea991f5fcf17abe7b3db8e66c674c1
andreasbandersson <andreasbjornandersson@gmail.com>
2019-12-18 15:38:37 +0100

    severe update of html and css, some changes in javascript

--- Frontend/javascript/main.js
+++ Frontend/javascript/main.js
@@ -6,30 +6,18 @@ $(document).ready(function() {
     });
 
     $('.button').click(function(){
-    	if(start){
-    		$('#main').addClass('animation');
-    		$('.button').html("STOP")
-    		start = false;
-    	}else{
-    		$('#main').removeClass('animation');
-    		$('.button').html("DISCO TIME")
-        	start = true;
-    	}
-    });
-    var btn = $('.musicbutton');
-    
-    btn.click(function() {
-    
-        btn.toggleClass('paused');
-    
-    return false;
+        if(start){
+            $('body').addClass('animation');
+            $('.button').html("STOP")
+            start = false;
+        }else{
+            $('body').removeClass('animation');
+            $('.button').html("DISCO TIME")
+            start = true;
+        }
     });
     
 });    
-
-function menuButton(x) {
-  x.classList.toggle("change");
-}
 
 function disco(element) {
     $(element).html("A nine volt battery from japan It's got the beat A little tape deck in my hand It's so so sweet Keeps me rockin' 'round the clock I don't mind it I got the beatbox, the beatbox, Just a beatin' that box Beatbox, beatbox One see-90's back to back Jumpin' on that jack Don't play with fire or Unplug my wire, beg or steal It's not for hire Keeps me rockin' 'round the clock I don't…");
